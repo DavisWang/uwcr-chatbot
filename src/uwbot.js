f5fdd1fa785f6c2c839e26030c43cc3a780bc886
Davis Wang <davis@daviswang.com>
2014-05-13 21:20:41 -0400

    Add next holiday, removed parsedResponse var

--- src/uwbot.js
+++ src/uwbot.js
@@ -13,9 +13,24 @@ function process (command, callback) {
   var args = command.split(" ");
   switch(args[1]) {
     case "weather":
-      getWeather(function (data) {
-        callback(data);
-      });
+      if(args.length == 2) {
+        getWeather(function (data) {
+          callback(data);
+        });
+      }
+      else {
+        callback(returnHelpString());
+      }
+      break;
+    case "holiday":
+      if(args.length == 2) {
+        getNextHoliday(function (data) {
+          callback(data);
+        });
+      }
+      else {
+        callback(returnHelpString());
+      }
       break;
     case "exam":
       if(args.length == 4) {
@@ -32,6 +47,7 @@ function process (command, callback) {
         <b>UWBot commands:</b> <br> \
           <b>weather</b>: get the current weather in waterloo <br> \
           <b>exam</b> (subject) (course_num): Gets exam info for given subject <br> \
+          <b>holiday</b>: Get the date of the next holiday! <br> \
           <b>disclaimer</b>: Prints a boring disclaimer <br> \
           <b>help</b>: print this help command <br>");
       break;
@@ -48,29 +64,45 @@ function returnHelpString() {
   return "Unrecognized command, please refer to '@bot help' for accepted command.";
 }
 
+function getNextHoliday(callback) {
+  var url = "/v2/events/holidays.json";
+  sendReq(url, function (response) {
+    var holiday;
+    var date = new Date();
+    for (index in response.data) {
+      if(new Date(response.data[index].date) > date) {
+        holiday = response.data[index];
+        break;
+      }
+    }
+    if(typeof holiday !== "undefined") {
+      callback("The next holiday is " + holiday.name + " on " + holiday.date + ". Yay!");
+    }
+    else {
+      callback("Cannot find the next holiday, maybe there is none... :(");
+    }
+  });
+}
+
 function getCurrentTerm(callback) {
   var url = "/v2/terms/list.json" + "?key=" + key;
-  var parsedResponse;
   sendReq(url, function (response) {
-    parsedResponse = JSON.parse(response);
-    callback(parsedResponse.data.current_term);
+    callback(response.data.current_term);
   });
 }
 
 function getExamSchedule(subj, num, callback) {
   var url = "/v2/courses/" + subj + "/" + num + "/examschedule.json" + "?key=" + key;
   var responseStr;
-  var parsedResponse;
   sendReq(url, function (response) {
-    parsedResponse = JSON.parse(response);
-    if(typeof parsedResponse.data.course !== "undefined") {
-      responseStr = "Exam Info for " + parsedResponse.data.course + ": <br>";
-      for(section in parsedResponse.data.sections) {
-        responseStr += "Section: " + parsedResponse.data.section.section + "<br> \
-        Date: " + parsedResponse.data.section.date + "<br> \
-        Start Time: " + parsedResponse.data.section.start_time + "<br> \
-        End Time: " + parsedResponse.data.section.end_time + "<br> \
-        Location: " + parsedResponse.data.section.location + "<br> \
+    if(typeof response.data.course !== "undefined") {
+      responseStr = "Exam Info for " + response.data.course + ": <br>";
+      for(section in response.data.sections) {
+        responseStr += "Section: " + response.data.section.section + "<br> \
+        Date: " + response.data.section.date + "<br> \
+        Start Time: " + response.data.section.start_time + "<br> \
+        End Time: " + response.data.section.end_time + "<br> \
+        Location: " + response.data.section.location + "<br> \
         ";
       }
     }
@@ -84,10 +116,8 @@ function getExamSchedule(subj, num, callback) {
 function getWeather(callback) {
   var url = "/v2/weather/current.json";
   var responseStr;
-  var parsedResponse;
   sendReq(url, function (response) {
-    parsedResponse = JSON.parse(response);
-    responseStr = "The current temperature in Waterloo is: " + parsedResponse.data.temperature_current_c + " Celsius";
+    responseStr = "The current temperature in Waterloo is: " + response.data.temperature_current_c + " Celsius";
     callback(responseStr);
   });
 }
@@ -111,7 +141,7 @@ function sendReq(url, callback) {
     bodyChunks.push(chunk);
   }).on('end', function() {
     var body = Buffer.concat(bodyChunks);
-    callback(body);
+    callback(JSON.parse(body));
   });
   });
 
