af0444d7ead6587cc4c54f8f7414abc05b7dce4e
Jason <jason810118@gmail.com>
2014-05-21 15:39:15 -0400

    Revert changes to test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var bot = require("./src/uwbot.js")
 
 //replace weather with command
-var command = "@bot";
+var command = "@bot " + "weather";
 
 bot.process(command, function (data) {
 	console.log(data);
